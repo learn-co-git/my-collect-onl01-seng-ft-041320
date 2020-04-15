34ab873f1fadf31a9dfb4ffb86fc71b5bbb169fc
Neil Empey <argus.two.2@gmail.com>
2020-04-15 16:36:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,13 @@
 
 def my_collect(array)
+  new_array = []
   if(array.empty?)
     return array 
   end 
   i = 0 
   while i < array.length 
-  yield(array[i])
+  new_array << yield(array[i])
   i += 1 
 end 
-return array 
+return new_array 
 end 
